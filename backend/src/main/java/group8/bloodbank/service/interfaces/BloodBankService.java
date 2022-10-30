9b0a09a212b310fe0988e0b58f00531eae4651c5
todorbelic <belictodor@gmail.com>
2022-10-30 21:23:55 +0100

    'added_api_auth'

--- backend/src/main/java/group8/bloodbank/service/interfaces/BloodBankService.java
+++ backend/src/main/java/group8/bloodbank/service/interfaces/BloodBankService.java
@@ -11,8 +11,6 @@ public interface BloodBankService {
     public List<BloodBank> getAll();
     public BloodBank getById(Long id);
     public boolean getAmountOfBloodForType(BloodType type, Long id);
-    public Optional<Double> getAmountOfBloodForType(BloodType type, int id);
     public void setApiKey(String apiKey, Long id);
-    
     public BloodBank getByApiKey(String apiKey);
 }
