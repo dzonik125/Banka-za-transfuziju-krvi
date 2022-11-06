8af8bbb98a26b77740c33c7cacda7f8d2a472da5
davidmijailovic <mijailovicd00@gmail.com>
2022-11-06 22:00:24 +0100

    added rabbitmq configuration

---
+++ backend/src/main/java/group8/bloodbank/rabbitmq/ScheduledTasks.java
@@ -0,0 +1,29 @@
+package group8.bloodbank.rabbitmq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+@EnableScheduling
+public class ScheduledTasks {
+
+    private final RabbitMQSender rabbitMQSender;
+
+    @Autowired
+    public ScheduledTasks(RabbitMQSender rabbitMQSender) {
+        this.rabbitMQSender = rabbitMQSender;
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void sendMessage() {
+        MessageDto message = new MessageDto("Random message from Java client: " + UUID.randomUUID().toString());
+        rabbitMQSender.send(message);
+        System.out.println("Sent :" + message.toString());
+    }
+}
