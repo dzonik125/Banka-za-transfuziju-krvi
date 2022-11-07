4e00dbe4f7cd85f29f402127ce241938f8dfeb65
Nikola Kolarov <dzonik125@gmail.com>
2022-11-07 15:12:30 +0100

    Added userProfile view

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UserProfileViewComponent } from './modules/public/user-profile-view/use
 import { AdminDashboardComponent } from './modules/administrator/admin-dashboard/admin-dashboard.component';
 import { RegisterBloodBankComponent } from './modules/administrator/register-blood-bank/register-blood-bank.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
