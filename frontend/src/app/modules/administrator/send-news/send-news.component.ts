5e4553a786361e937106148cc61c39dfd5e9be2b
Nikola Kolarov <dzonik125@gmail.com>
2022-11-08 03:10:17 +0100

    implemented send news front to back

--- frontend/src/app/modules/administrator/send-news/send-news.component.ts
+++ frontend/src/app/modules/administrator/send-news/send-news.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Message } from 'src/app/model/message';
+import { Buffer } from 'buffer';
+
+window.Buffer = Buffer;
 
 @Component({
   selector: 'app-send-news',
@@ -6,26 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./send-news.component.css']
 })
 export class SendNewsComponent implements OnInit {
-
-  subject: any;
-  message: any;
-  text: any;
-
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
-  url="./assets/bb.png";
+
+  url="";
+  subject = '';
+  text = '';
 
   onFileChanged(event: any) {
     if(event.target.files){
     var reader = new FileReader();
     reader.readAsDataURL(event.target.files[0]);
+    let byteArray;
     reader.onload = (event: any) => {
        this.url = event.target.result;
     }
   }
+}
+
+convertDataURIToBinary(dataURI: string): Uint8Array {
+  var base64Index = dataURI.indexOf(';base64,') + ';base64,'.length;
+  var base64 = dataURI.substring(base64Index);
+  var raw = window.atob(base64);
+  var rawLength = raw.length;
+  var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+  for(let i = 0; i < rawLength; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  return array;
+}
+
+sendNews(){
+  var base64string = Buffer.from(this.url).toString('base64');
+  var msg = new Message(this.text, this.subject, base64string);
+  var headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  this.http.post('http://localhost:8081/sendNews', msg, { headers }).subscribe(res => {
+    console.log(res);
+  });
 }
 
 }
