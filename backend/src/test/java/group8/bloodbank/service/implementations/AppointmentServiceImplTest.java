f13108c3c1f10a2149f86db71ad5def037ad594c
unknown <dzonik125@gmail.com>
2023-01-26 15:45:22 +0100

    scalabilty proof of concept

--- backend/src/test/java/group8/bloodbank/service/implementations/AppointmentServiceImplTest.java
+++ backend/src/test/java/group8/bloodbank/service/implementations/AppointmentServiceImplTest.java
@@ -20,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -49,10 +51,41 @@ class AppointmentServiceImplTest {
         Appointment appointment = new Appointment(15L, medicalWorkerService.getAll(), Long.parseLong("1"), bloodBankService.getById(Long.parseLong("1")).get(), start, 30.0, donorService.getAll().get(0));
         Appointment appointment2 = new Appointment(30L, medicalWorkerService.getAll(), Long.parseLong("1"), bloodBankService.getById(Long.parseLong("1")).get(), start, 30.0, donorService.getAll().get(1));
 
-        boolean first = appointmentService.scheduleAppointment(appointment);
-        TimeUnit.SECONDS.sleep(5);
-        boolean second = appointmentService.scheduleAppointment(appointment2);
-        assertEquals(first, true);
-        assertEquals(second, false);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        final boolean[] first = {false};
+        final boolean[] second = {true};
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Startovan thread 1");
+                try {
+                    first[0] = appointmentService.scheduleAppointment(appointment);
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (WriterException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Startovan thread 2");
+                try{Thread.sleep(150);} catch (InterruptedException e) {}
+                try {
+                    second[0] = appointmentService.scheduleAppointment(appointment2);
+                    assertEquals(second[0], false);
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (WriterException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 }
